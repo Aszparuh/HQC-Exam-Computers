f48907d4dbd1d3a2c32272406fbfbee4e66c3c69
puhabg <puhabg@live.com>
2016-10-03 13:34:32 +0300

    Adds abstract class for Cpu

---
+++ Source/Models/Computers.Models/Components/BaseCpu.cs
@@ -0,0 +1,14 @@
+﻿namespace Computers.Models.Components
+{
+    internal abstract class BaseCpu
+    {
+        private readonly byte numberOfCores;
+
+        internal BaseCpu(byte numberOfCores)
+        {
+            this.numberOfCores = numberOfCores;
+        }
+
+        internal abstract int SquareNumber();
+    }
+}
