edddc6ada5ee6fec1c798f997c7bb2508a368acb
puhabg <puhabg@live.com>
2016-10-04 12:51:14 +0300

    Adds battery

---
+++ Source/Models/Computers.Models/Components/Batteries/IBatttery.cs
@@ -0,0 +1,9 @@
+﻿namespace Computers.Models.Components.Batteries
+{
+    public interface IBatttery
+    {
+        int Percentage { get; }
+
+        void Charge(int percentage);
+    }
+}
---
+++ Source/Models/Computers.Models/Components/Batteries/LaptopBattery.cs
@@ -0,0 +1,30 @@
+﻿namespace Computers.Models.Components.Batteries
+{
+    public class LaptopBattery
+    {
+        private const int InitialCharge = 50;
+        private const int MinimalCharge = 0;
+        private const int MaximalCharge = 100;
+
+        public LaptopBattery()
+        {
+            this.Percentage = InitialCharge;
+        }
+
+        public int Percentage { get; private set; }
+
+        public void Charge(int percentage)
+        {
+            this.Percentage += percentage;
+            if (this.Percentage > MaximalCharge)
+            {
+                this.Percentage = 100;
+            }
+
+            if (this.Percentage < MinimalCharge)
+            {
+                this.Percentage = 0;
+            }
+        }
+    }
+}
